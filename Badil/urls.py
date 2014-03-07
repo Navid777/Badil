056cfca8b1565519ddfdd7c2c14b2ce07b82c03b
FardadJalili <jalili.fardad@gmail.com>
2014-03-07 20:01:44 +0330

    Reformat

--- Badil/urls.py
+++ Badil/urls.py
@@ -3,21 +3,22 @@ from book.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Badil.views.home', name='home'),
-    # url(r'^Badil/', include('Badil.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'Badil.views.home', name='home'),
+                       # url(r'^Badil/', include('Badil.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
-     url(r'^unpublished/$', unpublished),
-     url(u'^bookByFirstChar/(?P<first>\w{1})/$', booksByFirstCharacter),
-     url(u'^personByFirstChar/(?P<first>\w{1})/$', publishedPersonsByFirstCharacter),
-     url(r'^bookBySubject/(?P<id>\d{1})/$', booksBySubject),
-     url(r'^unpublishedBySubject/(?P<id>\d{1})/$', unpublishedBooksBySubject),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^unpublished/$', unpublished),
+                       url(u'^bookByFirstChar/(?P<first>\w{1})/$', books_by_first_character),
+                       url(u'^personByFirstChar/(?P<first>\w{1})/$', published_persons_by_first_character),
+                       url(r'^bookBySubject/(?P<id>\d{1})/$', books_by_subject),
+                       url(r'^unpublishedBySubject/(?P<id>\d{1})/$', unpublished_books_by_subject),
 )
--- book/views.py
+++ book/views.py
@@ -4,27 +4,34 @@ from book.models import Person
 from models import Book
 from django.db.models import Q
 
+
 def unpublished(request):
-    unpublishedBooks = Book.objects.filter(isPublished = False)
-    for book in unpublishedBooks :
-        print(unpublishedBooks[1].picture.__str__())
+    unpublished_books = Book.objects.filter(isPublished=False)
+    for book in unpublished_books:
+        print(unpublished_books[1].picture.__str__())
     return HttpResponse("unPublished Books are on 'unpublishedBooks' array.")
 
-def booksByFirstCharacter(request, first):
-    searchResults = Book.objects.filter(title__startswith = first)
-    return HttpResponse(searchResults[0].title)
 
-def publishedPersonsByFirstCharacter(request, first):
-    authorResults = Person.objects.filter(Q(authors__isnull = False),
-                                          Q(firstName__startswith = first) | Q(lastName__startswith = first)).distinct()
-    translatorResults = Person.objects.filter(Q(translators__isnull = False),
-                                          Q(firstName__startswith = first) | Q(lastName__startswith = first)).distinct()
-    return HttpResponse(authorResults[0].firstName)
+def books_by_first_character(request, first):
+    search_results = Book.objects.filter(title__startswith=first)
+    return HttpResponse(search_results[0].title)
 
-def booksBySubject(request, id):
-    bookResults = Book.objects.filter(isPublished=True,subjects__id = id)
-    return HttpResponse(bookResults[0].title)
 
-def unpublishedBooksBySubject(request, id):
-    bookResults = Book.objects.filter(isPublished = False, subjects__id = id)
-    return HttpResponse(bookResults[0].title)
+def published_persons_by_first_character(request, first):
+    author_results = Person.objects.filter(Q(authors__isnull=False),
+                                           Q(firstName__startswith=first) |
+                                           Q(lastName__startswith=first)).distinct()
+    translator_results = Person.objects.filter(Q(translators__isnull=False),
+                                               Q(firstName__startswith=first) |
+                                               Q(lastName__startswith=first)).distinct()
+    return HttpResponse(author_results[0].firstName)
+
+
+def books_by_subject(request, id):
+    book_results = Book.objects.filter(isPublished=True, subjects__id=id)
+    return HttpResponse(book_results[0].title)
+
+
+def unpublished_books_by_subject(request, id):
+    book_results = Book.objects.filter(isPublished=False, subjects__id=id)
+    return HttpResponse(book_results[0].title)
